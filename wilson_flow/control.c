1662e37e1f4d42637095dff43e73c3b1b3b8f573
David Schaich <daschaich@gmail.com>
2016-05-13 23:45:28 -0400

    Checkpoint scheme of adjusting step size so as to land on integer c/0.05

--- wilson_flow/control.c
+++ wilson_flow/control.c
@@ -2,6 +2,7 @@
 // Main procedure for SU(3) Wilson flow
 #define CONTROL
 #include "wflow_includes.h"
+#define TOL 1e-6        // For floating-point comparisons
 // -----------------------------------------------------------------
 
 
@@ -91,10 +92,10 @@ void mcrg_block(Real t, int blmax) {
 int main(int argc, char *argv[])  {
   register int i, dir;
   register site *s;
-  int j, prompt, istep, block_count = 0, blmax = 0, L;
-  double dtime, t = 0, E, old_value, new_value = 0, der_value;
-  double ssplaq, stplaq, plaq, check, topo;
-  double oldPlaq, dPlaq = -1.0, old_dPlaq = -1.0;
+  int j, prompt, istep, block_count = 0, blmax = 0, L, count;
+  double dtime, t = 0.0, E, old_value, new_value = 0.0, der_value;
+  double ssplaq, stplaq, plaq, check, topo, eta = 0.0;
+  double oldPlaq, dPlaq = -1.0;
   double magicC = 0.05, magicT;
   complex tc;
   su3_matrix t_mat, *S[4];
@@ -137,23 +138,33 @@ int main(int argc, char *argv[])  {
     blmax++;
   }
 
-  // Set first magicT = (0.05L)^2 / 8, with L = min(nx, nt) set above
-  // Special case: Make sure we don't pass this before resetting epsilon
-  magicT = magicC * magicC  * L * L / 8.0;
-  while (magicT < 2.0 * start_eps) {
-    magicC += 0.05;
-    magicT = magicC * magicC * L * L / 8.0;
-  }
-
   // Special case: measure MCRG-blocked observables before any flow
   if (num_block > 0 && tblock[0] == 0) {
     mcrg_block(tblock[block_count], blmax);
     block_count++;
   }
 
-  // Wilson flow!
-  epsilon = start_eps;
-  node0_printf("EPS %g\n", epsilon);
+  // Only adjust epsilon if start_eps < max_eps
+  if (fabs(start_eps - max_eps) > TOL) {
+    // Find the first magicT = (cL)^2 / 8 not within the first ten steps,
+    // with L = min(nx, nt) set above
+    magicT = magicC * magicC  * L * L / 8.0;
+    while (magicT < 10.0 * start_eps) {
+      magicC += 0.05;
+      magicT = magicC * magicC * L * L / 8.0;
+    }
+
+    // Figure out how many steps until the next magicT
+    // Then add one more and reduce epsilon to land bang on magicT
+    count = ceil(magicT / start_eps) + 1;
+    epsilon = magicT / (Real)count;
+    node0_printf("EPS %g to reach t = %g for c = %g in %d steps\n",
+                 epsilon, magicT, magicC, count);
+  }
+  else
+    epsilon = max_eps;
+
+  // Main flow loop
   plaquette(&ssplaq, &stplaq);
   oldPlaq = 0.5 * (ssplaq + stplaq);
   for (istep = 0;
@@ -209,8 +220,6 @@ int main(int argc, char *argv[])  {
     node0_printf("WFLOW %g %g %g %g %g %g %g\n",
                  t, plaq, E, new_value, der_value, check, topo);
 
-
-
     // For the tmax == 0 case, figure out if we can stop
     // Never stop before t=1
     // stepi%20 == 0 suppresses fluctuations in the flow length
@@ -220,23 +229,47 @@ int main(int argc, char *argv[])  {
         break;
     }
 
-    // Reset epsilon when appropriate
-    dPlaq = fabs(plaq - oldPlaq);         // Make it positive!
-    if (istep > 9 && epsilon < max_eps) {
-      epsilon *= old_dPlaq / dPlaq;
-      if (epsilon > max_eps)
-        epsilon = max_eps;
-      node0_printf("EPS %g\n", epsilon);
-    }
-    oldPlaq = plaq;
-    old_dPlaq = dPlaq;
-
-    // Do MCRG blocking at specified t, given new value of epsilon
+    // Do MCRG blocking at specified t
+    // Since epsilon should only increase, this can go before the adjustment
     if (block_count < num_block
         && fabs(t + 0.5 * epsilon) >= fabs(tblock[block_count])) {
       mcrg_block(tblock[block_count], blmax);
       block_count++;
     }
+
+    // Adjust epsilon when appropriate
+    count--;
+    dPlaq = fabs(plaq - oldPlaq);         // Force positivity
+
+    // Average over the first ten epsilon * dPlaq to define eta
+    if (istep < 10)
+      eta += 0.1 * epsilon * dPlaq;
+
+    // Only adjust epsilon if start_eps < max_eps
+    if (count == 0 && fabs(epsilon - max_eps) > TOL) {
+      // We should be at the magicT we wanted
+      if (fabs(t - magicT) > TOL)
+        node0_printf("WARNING: wanted t=%g but have %g\n", magicT, t);
+
+      // Just end if we're already done
+      if (fabs(tmax - t) < 0.5 * fabs(epsilon))
+        break;
+
+      epsilon = eta / dPlaq;
+      if (epsilon > max_eps)
+        epsilon = max_eps;
+
+      // Figure out how many steps until the next magicT
+      // Then add one more and reduce epsilon to land bang on magicT
+      // Recall L = min(nx, nt)
+      magicC += 0.05;
+      magicT = magicC * magicC * L * L / 8.0;
+      count = ceil((magicT - t) / start_eps) + 1;
+      epsilon = (magicT - t) / (Real)count;
+      node0_printf("EPS %g to reach t = %g for c = %g in %d steps\n",
+                   epsilon, magicT, magicC, count);
+    }
+    oldPlaq = plaq;
   }
 
   node0_printf("RUNNING COMPLETED\n");
